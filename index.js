2b5389247a5931854032526df883aaba4d85a207
Lucas Ribeiro <lucasribeiro1024b@gmail.com>
2022-04-27 23:30:20 -0300

    work: villain vs hero

---
+++ index.js
@@ -0,0 +1,13 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+const villain = supervillains.random();
+const hero = superheroes.random();
+
+const versus = {
+  'hero': hero,
+  'villain': villain
+}
+
+console.log(versus);
+console.log(versus.hero + ' VS ' + versus.villain);
